9cde3b226305201f3f6bfcfa78c56e448164397d
Rene Montilva <renemontilva@gmail.com>
2022-10-04 23:41:56 +0200

    feat(var): Add vpc_name variable

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
     cidr_block = var.cidr_block
 
     tags = {
-        Name = "Test VPC"
+        Name = var.vpc_name
     }
   
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
     type = string
     description = "ip address block for a vpc."
 }
+
+variable "vpc_name" {
+    type = string
+    description = "Vpc tag name"
+}
